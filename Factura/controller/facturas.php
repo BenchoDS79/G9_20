5a8e3244ac689da13303b6c990dd75fae6f5186e
BenchoDS79 <durons.ruben79@gmail.com>
2021-12-10 00:27:27 -0600

    02

--- Factura/controller/facturas.php
+++ Factura/controller/facturas.php
@@ -21,29 +21,29 @@
 
     switch($_GET["op"]){
         
-    case "GetFacturas":
-      $datos=$facturas->get_facturas();
-      echo json_encode($datos);
-     break;
+        case "GetFacturas":
+            $datos=$facturas->get_facturas();
+            echo json_encode($datos);
+        break;
 
-     case "GetUno": 
-        $datos=$facturas->get_factura($body["ID"]);
-        echo json_encode($datos);
-     break;
+        case "GetUno": 
+            $datos=$facturas->get_factura($body["ID"]);
+            echo json_encode($datos);
+        break;
      
-     case "InsertFactura":
-        $datos=$facturas->insert_factura($body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"]);
-        echo json_encode("Factura Agragada");
-    break;
+        case "InsertFactura":
+            $datos=$facturas->insert_factura($body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"]);
+            echo json_encode("Factura Agragada");
+        break;
 
-    case "UpdateFactura":
-        $datos=$facturas->update_factura($body["ID"],$body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"]);
-        echo json_encode("Factura Actualizada");
-    break;
-    break;
-    case "DeleteFactura":
-        $datos=$facturas->delete_factura($body["ID"]);
-        echo json_encode("Factura Eliminada");
-    break;
+        case "UpdateFactura":
+            $datos=$facturas->update_factura($body["ID"],$body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"]);
+            echo json_encode("Factura Actualizada");
+        break;
+
+        case "DeleteFactura":
+            $datos=$facturas->delete_factura($body["ID"]);
+            echo json_encode("Factura Eliminada");
+        break;
     }
 ?>
